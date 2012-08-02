4558394e3e8447d14d880ec736ba06a4fac3e0b7
Salman Saghafi <salman.saghafi@me.com>
2012-08-01 22:55:54 -0400

    adding AluminumVersion.java

---
+++ minalloy/AluminumVersion.java
@@ -0,0 +1,7 @@
+package minalloy;
+public final class AluminumVersion {
+private AluminumVersion() {}
+public static final boolean experimental = false;
+public static int buildNumber() { return 1; }
+public static String version() { return "0.1"; }
+public static String buildDate() { return "2012/08/01 10:35 EST"; }}
