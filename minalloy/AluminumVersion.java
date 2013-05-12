d13bdd22aa4100e8c5e6dcaa99811a504725dfce
Tim Nelson <tn@cs.wpi.edu>
2013-05-12 19:59:22 -0400

    fixed backtracking

--- minalloy/AluminumVersion.java
+++ minalloy/AluminumVersion.java
@@ -4,4 +4,4 @@ private AluminumVersion() {}
 public static final boolean experimental = false;
 public static int buildNumber() { return 8; }
 public static String version() { return "0.8"; }
-public static String buildDate() { return "2013/05/12 18:00 EDT"; }}
+public static String buildDate() { return "2013/05/12 20:00 EDT"; }}
