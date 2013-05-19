607fcca69b386e5412c0e30a4a2294b71da7b0c3
Tim Nelson <tn@cs.wpi.edu>
2013-05-19 09:45:28 -0400

    version update

--- minalloy/AluminumVersion.java
+++ minalloy/AluminumVersion.java
@@ -2,6 +2,6 @@ package minalloy;
 public final class AluminumVersion {
 private AluminumVersion() {}
 public static final boolean experimental = false;
-public static int buildNumber() { return 8; }
-public static String version() { return "0.8"; }
-public static String buildDate() { return "2013/05/12 20:00 EDT"; }}
+public static int buildNumber() { return 9; }
+public static String version() { return "0.9"; }
+public static String buildDate() { return "2013/05/19 10:00 EDT"; }}
