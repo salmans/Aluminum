915ff3bc0cf05656ad7d6f4a77dccb695f959d09
Tim Nelson <tn@cs.wpi.edu>
2014-05-28 15:42:50 -0400

    version

--- minalloy/AluminumVersion.java
+++ minalloy/AluminumVersion.java
@@ -3,5 +3,5 @@ public final class AluminumVersion {
 private AluminumVersion() {}
 public static final boolean experimental = false;
 public static int buildNumber() { return 9; }
-public static String version() { return "0.9"; }
-public static String buildDate() { return "2013/05/19 10:00 EDT"; }}
+public static String version() { return "0.10"; }
+public static String buildDate() { return "2014/05/28 10:00 EDT"; }}
